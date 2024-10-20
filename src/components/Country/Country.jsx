a354bd55c42292c3cd9bfca4884cb05d47d8086d
KanchonBauri <kanchonbauri1998@gmail.com>
2024-10-20 10:55:49 +0600

    handle visited button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
     console.log(country)
     const { name, flags, population, language, maps, cca3 } = country;
+
+const [visited, setVisited] = useState(false);
+const handleVisited = () =>{
+    setVisited(!visited);
+    // console.log("hello")
+}
     return (
-        <div className="country" >
+        <div className={`country ${visited && 'visited'}`} >
             <img src={flags.png} alt="" />
-            <h3>Name: {name?.common} </h3>
+            <h3 style={{color: visited ? 'Purple' : 'black' }} >Name: {name?.common} </h3>
             <div className='objects'>
                 <p>Population: {population}</p>
                 <p>Language: {language} </p>
                 <a href={maps.googleMaps}>Map</a>
                 <p><small>Code:{cca3}</small></p>
             </div>
-            <button>Visited</button>
+            <button onClick={handleVisited} >{visited ? 'Visited' : 'Now Visited'}</button>
+            {
+                visited ? ' I have visited this country' : ' I want to visited'
+            }
         </div>
     );
 };
